321983ac031bf1b04a56953dd7766d29e8de92fc
rainool <203097192+rainool@users.noreply.github.com>
2025-08-21 11:51:58 +1000

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    internal class Account
+    {
+        //Instance variables
+        private string _name;
+        private double _balance = 0.0;
+
+        
+        
+        //Constructor
+        public Account(string name, double balance)
+        {
+            this._name = name;
+            this._balance = balance;
+        }   
+
+        
+        //validation methods
+        public bool validateDbl(string userInput)
+        {
+            return double.TryParse(userInput, out double validated);
+        }
+
+
+        
+        //methods
+        public void deposit(string input)
+        {
+            double depositAmount = Convert.ToDouble(input);
+            if (depositAmount >= 0)
+            {
+                _balance += depositAmount;
+                Console.WriteLine("Successfully deposited " + depositAmount.ToString("C", CultureInfo.GetCultureInfo("en-US"))
+                    + "\nCurrent account balance: " + _balance.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+            }
+            else if (depositAmount < 0)
+            {
+                Console.WriteLine("Invalid amount.");
+            }
+        }
+
+        
+        
+        public void withdraw(string input)
+        {
+            double withdrawalAmount = Convert.ToDouble(input);
+            if (withdrawalAmount >= 0 && withdrawalAmount <= _balance)
+            {
+                _balance -= withdrawalAmount;
+                Console.WriteLine("Successfully withdrawn " + withdrawalAmount.ToString("C", CultureInfo.GetCultureInfo("en-US"))
+                    + "\nCurrent account balance: " + _balance.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+            }
+            else if (withdrawalAmount > _balance)
+            {
+                Console.WriteLine("Insufficient funds."
+                    + "\nCurrent account balance: " + _balance.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+            }
+            else if (withdrawalAmount < 0)
+            {
+                Console.WriteLine("Please enter a valid amount");
+            }
+        }
+
+        
+        
+        public void print()
+        {
+            Console.WriteLine("Account Name: " + _name 
+                + "\nAccount Balance: " + _balance.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        }
+
+        
+        
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
---
+++ BankSystem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BankSystem.BankSystem;
+
+namespace BankSystem
+{
+    internal enum MenuOption
+    {
+        Withdraw,
+        Deposit,
+        Print,
+        Quit
+    }
+
+    internal class BankSystem
+    {
+        static void Main(string[] args)
+        {
+            Account jimCBA = new Account("Jim", 100.10);
+
+            MenuOption userOption;
+            do
+            {
+                userOption = ReadUserOption();
+
+                switch (userOption)
+                {
+                    case MenuOption.Withdraw:
+                        DoWithdraw(jimCBA);
+                        break;
+                    case MenuOption.Deposit:
+                        DoDeposit(jimCBA);
+                        break;
+                    case MenuOption.Print:
+                        DoPrint(jimCBA);
+                        break;
+                    case MenuOption.Quit:
+                        Console.WriteLine("Quitting.....");
+                        break;
+                }
+                Console.WriteLine("\n-----------------------------------\n");
+            } while (userOption != MenuOption.Quit);
+
+            static MenuOption ReadUserOption()
+            {
+                int choice;
+                do
+                {
+                    Console.WriteLine("Please choose an option:");
+                    Console.WriteLine("1. Withdraw");
+                    Console.WriteLine("2. Deposit");
+                    Console.WriteLine("3. Print");
+                    Console.WriteLine("4. Quit");
+                    Console.Write("Enter your choice (1-4): ");
+
+                    string input = Console.ReadLine();
+                    bool validInt = int.TryParse(input, out choice);
+
+                    if (validInt && choice >= 1 && choice <= 4)
+                    {
+                        return (MenuOption)(choice - 1);
+                    }
+                    Console.WriteLine("Invalid option. Please try again.\n");
+
+                } while (true);
+            }
+
+            static void DoWithdraw(Account account)
+            {
+                Console.WriteLine("Enter withdrawal amount: ");
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double amount))
+                {
+                    account.withdraw(input);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid amount.");
+                }
+            }
+
+            static void DoDeposit(Account account)
+            {
+                Console.WriteLine("Enter deposit amount: ");
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double amount))
+                {
+                    account.deposit(input);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid amount.");
+                }
+            }
+
+            static void DoPrint(Account account)
+            {
+                account.print();
+            }
+        }
+        
+    }
+}
+         
+        
